1fb312edc791edd1a985c2de52c3b86d13288575
Adam Filchoir <filchoiradam@gmail.com>
2025-03-25 15:59:49 +0800

    Chore simplify adjacent checking

--- src/utils/is-adjacent.go
+++ src/utils/is-adjacent.go
@@ -9,40 +9,55 @@ import (
 
 func IsAdjacent(startPosition string, endPosition string) (res bool){
 	re := regexp.MustCompile(`([A-Ja-j]+)(\d+)`)
-	match := re.FindStringSubmatch(endPosition)
+	match := re.FindStringSubmatch(startPosition)
 	startRow := strings.ToUpper(match[1])
 	startCol, _ := strconv.Atoi(match[2])
-	rowInIndex := common.Character_list_Map[startRow]
-	row := common.Number_list_arr[startCol-2:startCol+1]
+	startRowIndex := common.Character_list_Map[startRow]
 
-	if rowInIndex-1 >= 0 {
-		char := common.Character_list_Arr[rowInIndex-1]
+	// row := common.Number_list_arr[startCol-2:startCol+1]
 
-
-		for _, num := range row {
-			if startPosition == char+strconv.Itoa(num) {
-				return true
-			}
-		}
+	possibleMoves := [][]int{
+		{startRowIndex - 1, startCol - 1}, {startRowIndex - 1, startCol}, {startRowIndex - 1, startCol + 1}, // Top row
+		{startRowIndex, startCol - 1}, /* Current Row */ {startRowIndex, startCol + 1}, // Left & Right
+		{startRowIndex + 1, startCol - 1}, {startRowIndex + 1, startCol}, {startRowIndex + 1, startCol + 1}, // Bottom row
 	}
 
-	char := common.Character_list_Arr[rowInIndex]
-	for _, num := range row {
-		if startPosition == char+strconv.Itoa(num) {
-			return true
-		}
-	}
+	for _, move := range possibleMoves {
+		rowIndex, col := move[0], move[1]
 
-	if rowInIndex <=9 {
-		char := common.Character_list_Arr[rowInIndex+1]
-		for _, num := range row {
-			if startPosition == char+strconv.Itoa(num) {
+		if rowIndex >= 0 && rowIndex < 10 && col >=1 && col <= 10 {
+			pos := common.Character_list_Arr[rowIndex] + strconv.Itoa(col)
+			if endPosition ==  pos{
 				return true
 			}
 		}
 	}
 
 
+	// if rowInIndex-1 >= 0 {
+	// 	char := common.Character_list_Arr[rowInIndex-1]
 
+	// 	for _, num := range row {
+	// 		if startPosition == char+strconv.Itoa(num) {
+	// 			return true
+	// 		}
+	// 	}
+	// }
+
+	// char := common.Character_list_Arr[rowInIndex]
+	// for _, num := range row {
+	// 	if startPosition == char+strconv.Itoa(num) {
+	// 		return true
+	// 	}
+	// }
+
+	// if rowInIndex <=9 {
+	// 	char := common.Character_list_Arr[rowInIndex+1]
+	// 	for _, num := range row {
+	// 		if startPosition == char+strconv.Itoa(num) {
+	// 			return true
+	// 		}
+	// 	}
+	// }
 	return false
 }
