8d95532ab7bf87f2a95e7efde1c764c3ea28a1e8
Adam Filchoir <filchoiradam@gmail.com>
2025-04-24 15:41:59 +0800

    Chore: create how-to-use endpoint

--- src/handlers/how-to-use.go
+++ src/handlers/how-to-use.go
@@ -1,10 +1,46 @@
 package handlers
 
 import (
-	"log"
+	"main/src/utils"
 	"net/http"
 )
 
+type HowToUsePayload struct {
+	Title              string            `json:"title"`
+	CreatedBy          []string          `json:"created-by"`
+	WhereToNext        string            `json:"where-to-next"`
+	About              map[string]string `json:"about"`
+	AvailableEndpoints map[string]string `json:"available-endpoints"`
+}
+
 func HowToUse(w http.ResponseWriter, r *http.Request) {
-	log.Println("How TO use this program")
+
+	aboutSection := map[string]string{
+		"1": "This is a backend server made by Adam and Nazmi to play Cherkers using REST",
+		"2": "We created this mainly to learn how to use Golang and to learn backend development",
+		"3": "This is not a project that contributes to anyone in particular,",
+		"4": "It was instead our own passion of wanting to learn Golang and Backend Dev",
+	}
+
+	availableEndpointsSection := map[string]string{
+		"1": "/how-to-use",
+		"2": "/how-to-play",
+		"3": "/start-game/{user}",
+		"4": "/{gameid}",
+		"5": "/{gameid}/{user}",
+		"6": "/{gameid}/{user}/move/{start}/to/{end}",
+		"7": "/history/{gameid}",
+		"8": "/leaderboard",
+	}
+
+	p := HowToUsePayload{
+		AvailableEndpoints: availableEndpointsSection,
+		About:              aboutSection,
+		Title:              "Welcome to Backend Checkers also known as Dam Haji (Checkers in malay)",
+		WhereToNext:        r.Host + `/how-to-play`,
+		CreatedBy:          []string{"Adam", "Nazmi"},
+	}
+
+
+	utils.Serve(w, p)
 }
