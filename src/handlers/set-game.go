7d0795a801cd9b0af92847029089658e129b8be1
Adam Filchoir <filchoiradam@gmail.com>
2025-05-06 12:51:36 +0800

    init manually set game board with game id and template id for ease of testing

---
+++ src/handlers/set-game.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func SetGameBoard(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	gameId := vars["gameid"]
+	templateNum := vars["templatenum"]
+
+	fmt.Println(gameId, templateNum)
+
+}
