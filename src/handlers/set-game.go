388cb87fef4016bafc361f9b09020c0601d055a0
Adam Filchoir <filchoiradam@gmail.com>
2025-05-06 14:27:10 +0800

    add other boards samples

---
+++ src/handlers/set-game.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"fmt"
+	"main/src/utils"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func SetGameBoard(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	gameId := vars["gameid"]
+	templateNum := vars["templatenum"]
+
+	fmt.Println(gameId, templateNum)
+
+
+
+	
+
+	utils.SubstituteServe(w)
+}
